b5f48b8cb26e5366f58d0dba4afc44ee03bc3173
JGuibord <jeremyguibord@outlook.com>
2020-09-15 23:29:11 -0400

    Commit made at 23:29 on Sept. 15th

--- RocketElevators/assets/plugins/jquery/quote-form.js
+++ RocketElevators/assets/plugins/jquery/quote-form.js
@@ -9,25 +9,50 @@ $(function(){
         
         if(building == "Residential"){
         residential.removeClass("d-none");
+        commercial.addClass("d-none");
+        corporate.addClass("d-none");
+        hybrid.addClass("d-none");
+        console.log(building); 
+        }
+        
+        else if(building == "Commercial"){
+        commercial.removeClass("d-none");
+        residential.addClass("d-none");
+        corporate.addClass("d-none");
+        hybrid.addClass("d-none");
         console.log(building);
-}
+        }
 
-else if(building == "Commercial"){
-    commercial.removeClass("d-none");
-    residential.addClass("d-none");
-    console.log(building);
-}
+        else if(building == "Corporate"){
+        corporate.removeClass("d-none");
+        commercial.addClass("d-none");
+        residential.addClass("d-none");
+        hybrid.addClass("d-none");
+        console.log(building);
+        }
 
-else if(building == "Corporate"){
-    corporate.removeClass("d-none");
-    commercial.addClass("d-none");
-    console.log(building);
-}
-
-else if(building == "Hybrid"){
-    hybrid.removeClass("d-none");
-    corporate.addClass("d-none");
-    console.log(building);
-}
+        else if(building == "Hybrid"){
+        hybrid.removeClass("d-none");
+        corporate.addClass("d-none");
+        residential.addClass("d-none");
+        commercial.addClass("d-none");
+        console.log(building);
+        }
 });
+
+function ResidentialData() {
+    var RESapartments = parseInt($("#num-apartments").val(), 10);
+    var RESfloors = parseInt($("#num-floors").val(), 10);
+    var RESavgAptFlr = Math.ceil(RESapartments / RESfloors);
+    var RESelevPerAptPerFlr = Math.ceil(RESavgAptFlr / 6);
+    var REScolumnPerFlr = RESfloors / 20;
+    var RESaddColumn = Math.floor(REScolumnPerFlr + 1);
+    var REStotalElev = Math.ceil(RESaddColumn) * RESavgAptFlr;
+    var RESelevCost = REStotalElev * SelectElevatorLine() ||0;
+    var RESinstallFees = REStotalElev * GetInstallationFees() ||0;
+    var REStotalPrice = RESelevCost * RESinstallFees ||0;
+
+$("#sub-total").text("$" + new Intl.NumberFormat().format(REStotalPrice.toFixed(2)));
+};
+
 });
